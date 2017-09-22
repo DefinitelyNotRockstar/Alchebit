4064f3c1b62d02cc9bbebee7cda263ccd62de6ee
Pablo Tomico Pérez <pablotomico@hotmail.com>
2017-09-23 00:28:44 +0100

    Implemented some basic AI for enemies

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public float speed;
+
+    private Transform playerTransform;
+
+    private void Awake() {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void FixedUpdate() {
+        this.transform.position += (playerTransform.position - this.transform.position).normalized * speed / 500;
+    }
+}
