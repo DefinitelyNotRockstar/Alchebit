1b4c765e903f3d6421960968f0b38d1e0c46d0ee
Jazzzy <gladislacebrafeliz@gmail.com>
2017-09-24 16:02:27 +0100

    Added smoke animation

---
+++ Assets/SpawnSmokeScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSmokeScript : MonoBehaviour {
+
+
+	private Animator animator;
+	private AudioSource audioSource;
+
+	private void Awake()
+	{
+		animator = GetComponent<Animator>();
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+		animator.Play("Spawn_Smoke");
+		audioSource.Play();
+		Destroy(transform.parent.gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
